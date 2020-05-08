41d90159200cf7377067fd474723ca6889af3f7b
Hugo Dias <hmdias@gmail.com>
2020-05-09 00:45:43 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
